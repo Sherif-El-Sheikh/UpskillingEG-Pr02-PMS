41f8c35048d7d780127df15eb086bdb3436495e1
abdalrahman <abdalrahmanshaip@gmail.com>
2024-05-15 13:39:38 +0300

    changePass-Module & dashboard

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -3,5 +3,6 @@ import LoadingSpinner from './LoadingSpinner'
 import SideBar from './SideBar'
 import Navbar from './Navbar'
 import RouteGuard from './RouteGuard'
+import ChangePass from './ChangePass'
 
-export { AuthForm, LoadingSpinner, SideBar, Navbar, RouteGuard }
+export { AuthForm, LoadingSpinner, SideBar, Navbar, RouteGuard, ChangePass}
