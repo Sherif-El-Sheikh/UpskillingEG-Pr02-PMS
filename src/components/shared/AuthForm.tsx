f51f03c928dddcecdc57294967b4d2d8b42448ac
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-05-15 03:03:44 +0300

    Login Form, Auth Context

--- src/components/shared/AuthForm.tsx
+++ src/components/shared/AuthForm.tsx
@@ -1,5 +1,19 @@
-const AuthForm = () => {
-  return <div>AuthForm</div>
+import Logo from '../../assets/images/PMS 3.svg'
+
+const AuthForm = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      {/* Logo */}
+      <div className='text-center mb-4'>
+        <img src={Logo} alt='logo' className='img-fluid logo' />
+      </div>
+      {/* Form */}
+      <div className='row auth-form p-5 rounded rounded-5'>
+        <span>Welcome to PMS</span>
+        {children}
+      </div>
+    </>
+  )
 }
 
 export default AuthForm
--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,13 @@ import './index.css'
 // bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+// Auth Provider
+import AuthProvider from './contexts/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 )
