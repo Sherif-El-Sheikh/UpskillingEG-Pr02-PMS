74911adee926a9a5fdd18e7d3d1309d797760ba0
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-05-19 17:34:19 +0300

    ListsForForms Context

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
 // bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+//bootstrap icons
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import './index.css'
 // Auth Provider
 import AuthProvider from './contexts/global/AuthContext'
 import ProjectsProvider from './contexts/modules/projects/projectsContext'
 import TasksProvider from './contexts/modules/tasks/tasksContext'
+import ListsForFormsProvider from './contexts/global/ListsForForms'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthProvider>
       <ProjectsProvider>
         <TasksProvider>
-          <App />
+          <ListsForFormsProvider>
+            <App />
+          </ListsForFormsProvider>
         </TasksProvider>
       </ProjectsProvider>
     </AuthProvider>
