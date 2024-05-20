8c7ee010e6e84448dbb0d9df74319572b9937004
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-05-20 14:21:08 +0300

    AddTask Form fixes
    - only buttons styles left

---
+++ src/components/shared/Form/AddEditForm.tsx
@@ -0,0 +1,130 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import { useListsForForms } from '../../../contexts/global/ListsForForms'
+import useTasksOperations from '../../../contexts/modules/tasks/tasksOperations'
+import './../styles/add-edit-Form.css'
+
+type FormData = {
+  title: string
+  description: string
+  employeeId: string
+  projectId: string
+}
+
+const AddEditForm: React.FC = () => {
+  const { allProjects, allUsers } = useListsForForms()
+  const { createTask } = useTasksOperations() // Destructure the createTask function from useTasksOperations
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<FormData>()
+
+  const onSubmit = handleSubmit((data) => {
+    createTask(data) // Use createTask function to handle form submission
+    reset() // Reset the form after submission
+  })
+
+  return (
+    <div className='add-edit-Form container w-75 bg-white p-5 rounded-5'>
+      <Form onSubmit={onSubmit}>
+        <Form.Group className='mb-3' controlId='formTitle'>
+          <Form.Label>Title</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Title'
+            {...register('title', { required: 'Title is required' })}
+          />
+          {errors.title && (
+            <span className='text-danger'>{errors.title.message}</span>
+          )}
+        </Form.Group>
+
+        <Form.Group className='mb-3' controlId='formDescription'>
+          <Form.Label>Description</Form.Label>
+          <Form.Control
+            as='textarea'
+            placeholder='Description'
+            rows={4}
+            {...register('description', {
+              required: 'Description is required',
+            })}
+          />
+          {errors.description && (
+            <span className='text-danger'>{errors.description.message}</span>
+          )}
+        </Form.Group>
+
+        <div className='row'>
+          <div className='col-md-6 mt-2'>
+            <Form.Label>User</Form.Label>
+            <Form.Select
+              {...register('employeeId', {
+                required: 'User is required',
+              })}
+            >
+              <option value=''>No Users Selected</option>
+              {allUsers.map((user: any) => (
+                <option key={user.id} value={user.id}>
+                  {user.userName}
+                </option>
+              ))}
+            </Form.Select>
+            {errors.employeeId && (
+              <span className='text-danger'>{errors.employeeId.message}</span>
+            )}
+          </div>
+
+          <div className='col-md-6 mt-2'>
+            <Form.Label>Project</Form.Label>
+            <Form.Select
+              {...register('projectId', { required: 'Project is required' })}
+            >
+              <option value=''>No Projects Selected</option>
+              {allProjects.map((project: any) => (
+                <option key={project.id} value={project.id}>
+                  {project.title}
+                </option>
+              ))}
+            </Form.Select>
+            {errors.projectId && (
+              <span className='text-danger'>{errors.projectId.message}</span>
+            )}
+          </div>
+        </div>
+
+        <div className='row mt-3'>
+          <div className='col-md-6'>
+            <Button
+              className='w-100 py-2 text-center rounded-5'
+              variant='light'
+            >
+              <Link
+                className='text-reset text-decoration-none'
+                to='/dashboard/tasks'
+              >
+                Cancel
+              </Link>
+            </Button>
+          </div>
+          <div className='col-md-6'>
+            <Button
+              type='submit'
+              className='w-100 py-2 text-center rounded-5'
+              variant='warning'
+            >
+              Save
+            </Button>
+          </div>
+        </div>
+      </Form>
+    </div>
+  )
+}
+
+export default AddEditForm
--- src/pages/Dashboard/AddTask.tsx
+++ src/pages/Dashboard/AddTask.tsx
@@ -1,15 +1,21 @@
-import MyForm from "./../../components/shared/Form/MyForm";
-import Header from './../../components/shared/Header';
-import { FaAngleLeft } from "react-icons/fa6";
+import AddEditForm from '../../components/shared/Form/AddEditForm'
+import Header from './../../components/shared/Header'
+import { FaAngleLeft } from 'react-icons/fa6'
 function AddTask() {
   return (
     <>
-    <div className=""><Header icon={<FaAngleLeft />} title={"View All Tasks"} mainTitle={"Add a New Task"}/></div>
-      <div className="container vh-100  pt-5">
-        <MyForm />
+      <div className=''>
+        <Header
+          icon={<FaAngleLeft />}
+          title={'View All Tasks'}
+          mainTitle={'Add a New Task'}
+        />
+      </div>
+      <div className='container pt-5'>
+        <AddEditForm />
       </div>
     </>
-  );
+  )
 }
 
-export default AddTask;
+export default AddTask
