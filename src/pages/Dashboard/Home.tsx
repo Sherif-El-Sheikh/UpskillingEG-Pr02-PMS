4fb0ed02a66d0373e152a1f4ea6ca69a2da62668
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-05-25 12:31:58 +0300

    Dashboard Banner

--- src/pages/Dashboard/Home.tsx
+++ src/pages/Dashboard/Home.tsx
@@ -1,5 +1,18 @@
+import { useAuthContext } from "../../contexts/global/AuthContext"
+
 const Home = () => {
-  return <div></div>
+  const { userData } = useAuthContext()
+  return (
+    <div className='p-4'>
+      <div className='banner d-flex flex-column justify-content-center align-items-start text-white fs-4 p-3'>
+        <span className='z-3'>
+          Hello <span className=" text-warning">{userData.userName}</span>, welcome to your dashboard
+        </span>
+        <p className='z-3 mt-2'>You can add projects and assign tasks to your team.</p>
+      </div>
+      <div className='charts'></div>
+    </div>
+  )
 }
 
 export default Home
